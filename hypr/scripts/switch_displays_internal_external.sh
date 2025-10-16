bb48b082fbde5929ded729a468125d430c827f87
Ali Ahmad <aliahmaddk@gmail.com>
2025-10-16 20:17:12 +0200

    Change external display to displayport.

--- hypr/scripts/switch_displays_internal_external.sh
+++ hypr/scripts/switch_displays_internal_external.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-if [[ "$(hyprctl monitors)" =~ "HDMI-A-1" ]] && [[ "$(hyprctl monitors)" =~ "eDP-1" ]]; then
-   hyprctl keyword monitor "eDP-1,disable"
-elif [[ "$(hyprctl monitors)" =~ "HDMI-A-1" ]]; then
-   hyprctl keyword monitor "HDMI-A-1,disable" && hyprctl keyword monitor "eDP-1,1920x1080,auto,1.25" 
-else 
-   hyprctl reload
-   if [[ "$(hyprctl monitors)" =~ "HDMI-A-1" ]]; then
-      hyprctl keyword monitor "eDP-1,disable" 
-   else
-      echo "No external monitors connected"
-   fi
+if [[ "$(hyprctl monitors all)" =~ "DP-2" ]] && [[ "$(hyprctl monitors)" =~ "eDP-1" ]]; then
+  hyprctl keyword monitor "DP-2,highres,auto,1"
+  hyprctl keyword monitor "eDP-1,disable"
+elif [[ "$(hyprctl monitors)" =~ "DP-2" ]]; then
+  hyprctl keyword monitor "eDP-1,highres,auto,1.25" 
+  hyprctl keyword monitor "DP-2,disable"
+else
+  echo "No external monitors connected"
 fi
