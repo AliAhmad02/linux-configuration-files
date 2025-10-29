0fddcea21ef41b75804b869c2cfb59751d72206f
Ali Ahmad <aliahmaddk@gmail.com>
2025-10-29 14:40:16 +0100

    Add Ruff to lsp-config.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -7,7 +7,7 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "asm_lsp", "rust_analyzer", "basedpyright" },
+      ensure_installed = { "lua_ls", "asm_lsp", "rust_analyzer", "basedpyright" , "ruff" },
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
@@ -23,12 +23,42 @@ return {
       vim.lsp.config("lua_ls", { capabilities = capabilities })
       vim.lsp.config("asm_lsp", { capabilities = capabilities })
       vim.lsp.config("rust_analyzer", { capabilities = capabilities })
-      vim.lsp.config("basedpyright", { capabilities = capabilities })
+      vim.lsp.config("basedpyright", {
+        capabilities = capabilities,
+        settings = {
+          basedpyright = {
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use ruff for linting
+              ignore = { '*' },
+            },
+          },
+        },
+      })
+      vim.lsp.config("ruff", { capabilities = capabilities })
       vim.lsp.enable("lua_ls")
       vim.lsp.enable("asm_lsp")
       vim.lsp.enable("rust_analyzer")
       vim.lsp.enable("basedpyright")
-    end,
+      vim.lsp.enable("ruff")
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
+          if client.name == 'ruff' then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end,
+        desc = 'LSP: Disable hover capability from Ruff',
+      })
+         end,
 
     keys = {
       { "gD",         vim.lsp.buf.declaration },
